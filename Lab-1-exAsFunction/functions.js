2e60ad1bdc8daa6d1276cd00b5a8800dc300953e
bnalepa <bnalepa@o2.pl>
2023-10-24 01:56:13 +0200

    Solved lab1 with functions

--- Lab-1-exAsFunction/functions.js
+++ Lab-1-exAsFunction/functions.js
@@ -4,18 +4,13 @@
  * Załóż, że w area jest zawsze liczba nieujemna. Zmienna PI jest w bibliotece Math
  */
 //let area = 3.14159;
-let area = 3.14159;
-let radius = '';
-radius = (Math.sqrt(area/Math.PI)).toFixed(6)
-/*
-    Wpisz kod zadania w miejscu tego komentarza.
-*/
-console.log(radius)
-if (radius === '1.000000'){
-    console.log("Test passed");
-} else {
-    console.log("Test failed");
+
+function circleArea(area)
+{ 
+    return Math.sqrt(area/Math.PI).toFixed(6)
 }
+
+console.log(circleArea(3.14159));
 
 /**
  * Napisz skrypt, który w łańcuchu triangle generuje ciąg znaków '#' i '\n' tworzących kształ trójkąta o wysokości 
@@ -31,16 +26,17 @@ if (radius === '1.000000'){
  * height = 0
  * height = 10
  */
-let height = 7;
+
+function Triangle(height)
+{
 let triangle = '';
 for(let i = 1; i<=height; i++)
 {
     triangle += "#".repeat(i) + "\n";
 }
-/*
-    Wpisz kod zadania w miejscu tego komentarza.
-*/
-console.log(triangle);
+    return triangle;
+}
+console.log(Triangle(7));
 
 /**
  * Napisz skrypt zamieniający wartość zmiennej decimalNumber  na liczbę rzymską w zakresie od 1 do 10.
@@ -48,60 +44,61 @@ console.log(triangle);
  * Jeśli decimalNumber nie zmieści sie w zakresie to umieść w romanNumber komunikat: Decimal number is out of range!
  * Jeśli decimalNumber jest równe undefined lub null to umieść w romanNumber komunikat: Decimal number is undefined or null!
  */
-let decimalNumber = 1;
-let romanNumber = 'IV';
-if(decimalNumber == null || decimalNumber == undefined){
 
-} else {
-    switch (decimalNumber){
-        case 1:
-        case 2:
-        case 3:
-            romanNumber = "I".repeat(decimalNumber);
-            break;
-        case 4:
-            romanNumber = "IV";
-            break;
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-            romanNumber = "V" +"I".repeat(decimalNumber%5);
-            break;
-        case 9:
-            romanNumber = "IX";
-            break;
-        case 10:
-            romanNumber = "X";
-            break;
-        default:
-            romanNumber = "Decimal number is out of range!";
+
+function changeDecimal(decimalNumber)
+{
+    let romanNumber = '';
+    if(decimalNumber == null || decimalNumber == undefined){
+
+    } else {
+        switch (decimalNumber){
+            case 1:
+            case 2:
+            case 3:
+                romanNumber = "I".repeat(decimalNumber);
+                break;
+            case 4:
+                romanNumber = "IV";
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                romanNumber = "V" +"I".repeat(decimalNumber%5);
+                break;
+            case 9:
+                romanNumber = "IX";
+                break;
+            case 10:
+                romanNumber = "X";
+                break;
+            default:
+                romanNumber = "Decimal number is out of range!";
+        }
     }
+    return romanNumber;
 }
 
-/*
-    Wpisz kod zadania w miejscu tego komentarza.
-*/
-console.log(romanNumber);
+console.log(changeDecimal(1));
 
 /**
  * Napisz skrypt, który generuje losowy łańcuch o długości w zmiennej length składający się ze znaków alfabetu a-z i cyfr.
  * Wygenerowany Łańcuch umieść w zmiennej randomString  
  */
 
-let length = 5;
-let randomString = '';
-let possibleChar = "abcdefghijklmnopqrstuvwxyz0123456789";
-
-for(let i = 0; i < length; i++)
+function generateRandomString(length)
 {
-    randomString += possibleChar[Math.round(Math.random(0)*36)];
+    let randomString = '';
+    let possibleChar = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for(let i = 0; i < length; i++)
+    {
+        randomString += possibleChar[Math.round(Math.random(0)*36)];
 }
-
-/*
-    Wpisz kod zadania w miejscu tego komentarza.
-*/
-console.log(randomString);
+    return randomString;
+}
+ 
+console.log(generateRandomString(5));
 
 /**
  * Napisz skrypt, który na podstawie dwóch liczb `n` < `m` tworzy łańcuch w zmiennej progressBar dwa wiersze:
@@ -116,18 +113,22 @@ console.log(randomString);
  */
 let n = 5;
 let m = 100;
-while(n<m)
+
+function bar(n, m)
 {
-    let progressBar = "";
-    let progression = Math.round(n/m*100);
-    progressBar += "|" + ("#".repeat(progression/2)).padEnd(50," ") + "|\n" // dodanie paska
-    progressBar += "0%".padEnd(24," ") + (progression + "%").padEnd(24," ") + "100%"   // dodanie procentów pod paskiem
-    /*
-        Wpisz kod zadania w miejscu tego komentarza.
-    */
-    console.log(progressBar);
-    n += 10
+    let progressBar;
+    while(n<m)
+    {
+        let progression = Math.round(n/m*100);
+        progressBar += "|" + ("#".repeat(progression/2)).padEnd(50," ") + "|\n" // dodanie paska
+        progressBar += "0%".padEnd(24," ") + (progression + "%").padEnd(24," ") + "100%\n"   // dodanie procentów pod paskiem
+
+        n += 10;
+    }
+    return progressBar;
 }
+console.log(bar(10.60, 100))
+
 
 /**
  * Napisz skrypt, który na podstawie zmiennych rectWidth i rectHeight stworzy łańcuch rectString zawierający pustą rankę o podanych wymiarach.
@@ -142,23 +143,27 @@ while(n<m)
  * #     #
  * #######  
  */
-let rectWidth = 3;
-let rectHeight = 10;
-let rectBorderSymbol = '#';
-let rectString = '';
 
-let regex = /\S/g;
-let check = rectBorderSymbol.match(regex);
-
-if(rectWidth >-1 && rectWidth < 81 && rectHeight >-1 && rectHeight < 81 && check != null && rectBorderSymbol.length == 1 )
+function border(rectWidth, rectHeight, rectBorderSymbol)
 {
-   rectString += rectBorderSymbol.repeat(rectWidth) +"\n";
-    for(let i = 2; i < rectHeight; i++)
+    let rectString = '';
+
+    let regex = /\S/g;
+    let check = rectBorderSymbol.match(regex);
+
+    if(rectWidth >-1 && rectWidth < 81 && rectHeight >-1 && rectHeight < 81 && check != null && rectBorderSymbol.length == 1 )
     {
-        rectString += rectBorderSymbol + " ".repeat(rectWidth-2) + rectBorderSymbol +"\n"; 
+    rectString += rectBorderSymbol.repeat(rectWidth) +"\n";
+        for(let i = 2; i < rectHeight; i++)
+        {
+            rectString += rectBorderSymbol + " ".repeat(rectWidth-2) + rectBorderSymbol +"\n"; 
+        }
+        rectString += rectBorderSymbol.repeat(rectWidth);
+    } else {
+        rectString = "Błędne dane wejściowe";
     }
-    rectString += rectBorderSymbol.repeat(rectWidth);
-} else {
-    rectString = "Błędne dane wejściowe"
+    return rectString;
 }
-console.log(rectString);
+console.log(border(5,10,'#'));
+
+
