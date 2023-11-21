bdb13fd40117001833a026279f334da403dade2e
bnalepa <bnalepa@o2.pl>
2023-11-21 11:21:49 +0100

    Initial Lab-6

---
+++ Lab-6/project/main.js
@@ -0,0 +1,9 @@
+document.onload = function(){
+    const links = [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtg6SNRxoTBGTHMxnV59khl2t1dAS0oynUMg&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCsXARN9tm6drVx30jwrEswb4IUa_5215ILw&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnZ-bY63aj4YNLsBiMm-BY2jjyXWVs9a1efw&usqp=CAU"
+    ]
+
+    const galery = document.getElementById("galery")
+}
